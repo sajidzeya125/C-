ca9022990436ada67923ac2330915d3692d280c9
sajidzeya125 <sajidzeya294@gmail.com>
2024-12-29 21:09:42 +0530

    Add files via upload

---
+++ bitwise_operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=6;
+    cout<<"a&b "<<(a&b)<<endl;
+     cout<<"a|b "<<(a|b)<<endl;
+      cout<<"~a "<<(~a)<<endl;
+       cout<<"a^b "<<(a^b)<<endl;
+       int i=9;
+       cout<<(++i)<<endl;
+       cout<<(i++)<<endl;
+       cout<<(--i)<<endl;
+       cout<<(i--)<<endl;
+
+}
